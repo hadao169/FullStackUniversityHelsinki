8b540a3cf93ad953719abb1f6bf556283ecb5882
Dao Thai Ha <daothaiha1692005@gmail.com>
2024-11-18 13:40:48 +0200

    Phonebook_Step6

--- PART_2/phonebook/src/App.jsx
+++ PART_2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
 const PersonForm = ({
   onAddName,
   onNameChange,
@@ -7,7 +10,7 @@ const PersonForm = ({
   newNumber,
 }) => {
   return (
-    <form onSubmit={onAddName}>
+    <form onSubmit={onAddName} className="form">
       <div>
         name: <input value={newName} onChange={onNameChange} />
       </div>
@@ -46,15 +49,17 @@ const Filter = ({ searchTerm, onSearch }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
