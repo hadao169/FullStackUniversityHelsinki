68ae2e93afd281a17eebdef5c2a837d7cdb80105
Dao Thai Ha <daothaiha1692005@gmail.com>
2024-12-07 03:08:20 +0200

    Phonebook backend step 6

--- PART_3/PhonebookBackend/index.js
+++ PART_3/PhonebookBackend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 const app = express();
-const port = 3005;
+const port = 3001;
+app.use(express.json());
 
-const phonebookList = [
+let phonebookList = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -27,6 +28,48 @@ const phonebookList = [
 app.get("/api/persons", (req, res) => {
   res.json(phonebookList);
 });
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(`
+    <p>Phonebook has info for ${phonebookList.length} entries.</p>
+    <p>${date}</p>
+  `);
+});
+
+app.post("/api/persons", (req, res) => {
+  let newID = Math.floor(Math.random() * 1000);
+  const existingIDs = phonebookList.map((person) => person.id);
+  while (existingIDs.includes(newID)) {
+    newID = Math.floor(Math.random() * 1000);
+  }
+  const newEntry = { id: newID.toString(), ...req.body };
+  phonebookList = [...phonebookList, newEntry];
+  res.json(phonebookList);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = phonebookList.find((phonebook) => phonebook.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const deletedID = req.params.id;
+  phonebookList = phonebookList.filter(
+    (phonebook) => phonebook.id !== deletedID
+  );
+  if (deletedID) {
+    res.json(phonebookList);
+  } else {
+    res.status(204).end();
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
